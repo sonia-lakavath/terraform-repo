2ec39ed2263f70c696caac73987189164ab557a1
sonia-lakavath <lakavath.sonia@globallogic.com>
2024-09-04 01:00:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,25 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 variable "region" {
-  description = "AWS region"
-  default     = "us-west-1"
+  description = "AWS region to deploy resources in"
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  default     = "10.0.1.0/24"
 }
 
 variable "instance_type" {
-  description = "Type of EC2 instance to provision"
+  description = "EC2 instance type"
   default     = "t2.micro"
 }
 
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Provisioned by Terraform"
+variable "ssh_cidr" {
+  description = "CIDR block for SSH access"
+  type        = string
+  default     = "0.0.0.0/0"
 }
-
