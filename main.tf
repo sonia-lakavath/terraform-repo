3cbbbb4767fb98ebbcff89d78c67fd811ed61799
sonia-lakavath <lakavath.sonia@globallogic.com>
2024-09-04 01:14:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,31 +1,110 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+# VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name = var.instance_name
+    Name = "main_vpc"
+  }
+}
+
+# Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "internet_gateway"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "route_table"
+  }
+}
+
+# Default Route
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+# Route Table Association
+resource "aws_route_table_association" "public_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow HTTP and SSH traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web_sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web_instance" {
+  ami           = "ami-0a91cd140a1fc148a"  # Amazon Linux 2 AMI (ap-south-1)
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.web_sg.name]
+
+  # Replace with your GitHub raw URL
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y git
+              git clone https://github.com/sonia-lakavath/terraform-repo.git /home/ec2-user/web-server-setup
+              chmod +x /home/ec2-user/web-server-setup/setup.sh
+              /home/ec2-user/web-server-setup/setup.sh
+              EOF
+
+  tags = {
+    Name = "WebServer"
   }
 }
