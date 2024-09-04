b9f8e83a1b44df07a950e029d61b59fdeed3f8f9
sonia-lakavath <lakavath.sonia@globallogic.com>
2024-09-04 17:43:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,28 +61,7 @@ resource "aws_security_group" "web_sg" {
   description = "Allow HTTP and SSH traffic"
   vpc_id      = aws_vpc.main_vpc.id
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.ssh_cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ 
 
   tags = {
     Name = "web_sg"
